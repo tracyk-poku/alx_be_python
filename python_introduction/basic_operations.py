9550b9d33ae112c2d5b34a3076b64a9f32b655a7
Tracy Poku <tracyk.poku@gmail.com>
2024-10-05 14:00:44 +0000

    Add python file with basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+x = number1 + number2
+y = number1 - number2
+z = number1 * number2
+
+print("Addition of" number1 "and" number2 "is" x)
+print("Subtraction of" number1 "and" number2 "is" y)
+print("Multiplication of" number1 "and" number2 "is" z)
